77aa4838995e76ee99bd94680e7606a9bc4cf2d4
FabyGiz <friveram@ucenfotec.ac.cr>
2025-06-22 16:29:00 -0600

    Agregar SP

---
+++ DataAccess/Database Scripts/SP_RET_MOVIES_BY_ID.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE RET_MOVIES_BY_ID_PR
+@P_ID INT
+AS
+BEGIN
+    SELECT  Id, Created, Updated, Title, Description, ReleaseDate, Genre, Director
+    FROM TBL_Movie
+    WHERE Id = @P_Id
+END
+GO
+
