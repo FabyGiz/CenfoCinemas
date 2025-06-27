dc92464c2bdb3ced83150f72f910578dedc1cb9e
FabyGiz <friveram@ucenfotec.ac.cr>
2025-06-26 20:40:44 -0600

    Primera parte clase 26-6

---
+++ WebApp1/wwwroot/js/pages/Users.js
@@ -0,0 +1,22 @@
+﻿// js que maneja todo el comportamiento de la vista de los usuarios
+
+//definir una clase js, usando prototype
+
+function UsersViewController() {
+
+    this.ViewName = "Users";
+    this.ApiEndPointName = "";
+
+    //metodo constructor
+    this.IntView = function () {
+
+        console.log("User init view --> ok");
+
+    }
+
+}
+
+$(document).ready(function (){  //con $ llamamos a JQuery
+    var vc = new UsersViewController();
+    vc.InitView();
+})
