dbcce98421ddcec2aee9f0c3b80a9ecbb7452c4a
FabyGiz <friveram@ucenfotec.ac.cr>
2025-07-05 10:19:13 -0600

    ajuste path api

--- WebApp1/wwwroot/js/pages/ControlActions.js
+++ WebApp1/wwwroot/js/pages/ControlActions.js
@@ -1,6 +1,6 @@
 ﻿function ControlActions() {
 	//Ruta base del API
-	this.URL_API = "https://localhost:7177/api/";
+	this.URL_API = "https://cenfocinemas-friveram-fqc7a0fwhchpdvdy.eastus2-01.azurewebsites.net/api/";
 
 	this.GetUrlApiService = function (service) {
 		return this.URL_API + service;
